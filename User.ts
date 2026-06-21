b2ac8e863b0ee0d9d677d8e90bd49255b82049bc
7on-it <295601178+7on-it@users.noreply.github.com>
2026-06-22 00:45:37 +1000

    Create User.ts

---
+++ User.ts
@@ -0,0 +1,43 @@
+class User {
+    // Статическое поле для подсчета экземпляров
+    static count: number = 0;
+    
+    private _name: string;
+    private _login: string;
+    private _password: string;
+    
+    // Конструктор
+    constructor(name: string, login: string, password: string) {
+        this._name = name;
+        this._login = login;
+        this._password = password;
+        
+        // Увеличиваем счетчик пользователей
+        User.count++;
+    }
+    
+    // Получить имя
+    get name(): string {
+        return this._name;
+    }
+  
+    // Изменить имя
+    set name(value: string) {
+        this._name = value;
+    }
+    
+    // Получить логин
+    get login(): string {
+        return this._login;
+    }
+    
+    // Изменить пароль
+    set password(value: string) {
+        this._password = value;
+    }
+    
+    // Вывод имени и логина
+    showInfo(): void {
+        console.log(`Пользователь: ${this._name}, Логин: ${this._login}`);
+    }
+}
